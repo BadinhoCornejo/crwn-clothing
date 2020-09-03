00c08056f527290033c07c0410454d1acfa3f032
Badinho <Cornejo>
2020-09-03 18:01:24 -0500

    finishing sign in handleSubmit methos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import "./App.css";
-
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
